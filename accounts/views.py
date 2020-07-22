b9c56ab418bedd559c5f178504083b9085f6dde9
adriano <adriano@localhost.localdomain>
2020-07-21 21:52:37 -0300

    dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,58 +1,35 @@
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-from .forms import UserForm
+
+
+from .forms import RegisterForm
 
 # Create your views here.
 
-def add_user(request):
+
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
+
+
+def register(request):
+    template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = UserForm(request.POST)
+        #cria um formulário passando os dados da requisição
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            u = form.save()
-            u.set_password(u.password)
-            u.save()
-            return HttpResponse('Usuário criado com sucesso!')
-            # return redirect('core:user_login')
-        else:
-            form = UserForm()
-            return HttpResponse('ERRO')
-        return render(request, 'accounts/add_user.html', { 'form':form })
-
-
-# def user_login(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#
-#         user = authenticate(username=username, password=password)
-#
-#         if user:
-#             login(request, user)
-#             #serve para quando o usuario quer entrar em uma página que exige login já continuar na mesma.
-#             return redirect(request.GET.get('next','/'))
-#         else:
-#             messages.error(request, 'Usuário ou senha inválida')
-#             #return redirect('accounts:user_login')
-#     return render(request, 'accounts/user_login.html')
-
-def user_logout(request):
-    logout(request)
-    return redirect('accounts:user_login')
-
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request,user)
-            messages.success(request,'Senha alterada com sucesso!')
-            return redirect('accounts:change_password')
-        else:
-            messages.error(request, 'Não foi possível alterar a sua senha')
+            form.save()
+            #chama o login url que está no settings
+            return redirect(settings.LOGIN_URL)
     else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'accounts/change_password.html', { 'form': form })
+        #cria um formulário vazio
+        form = RegisterForm()
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('contato', contact, name='contact')
 ]
