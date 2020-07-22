193ff2370cb60abd89e3f3d771c7ae51a2762155
AdrianoCarvalh0 <adriano.carvalho@ifsuldeminas.edu.br>
2020-07-22 14:13:58 -0300

    add edit

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,22 @@
 from django.conf import settings
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
 
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
 
 # Create your views here.
-
 
 
 @login_required
 def dashboard(request):
     template_name = 'accounts/dashboard.html'
+    # courses = Course.objects.all()
+    # context = {
+    #     'courses': courses
+    # }
     return render(request, template_name)
 
 
@@ -32,4 +35,35 @@ def register(request):
     context = {
         'form': form
     }
+    return render(request, template_name, context)
+
+@login_required
+def edit_user(request):
+    template_name = 'accounts/edit_user.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+
+@login_required
+def edit_password(request):
+    template_name = 'accounts/edit_password.html'
+    context = {}
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            context['success'] = True
+    else:
+        form = PasswordChangeForm(user=request.user)
+    context['form'] = form
     return render(request, template_name, context)
